1242352f10f518699527c966e6eeeac12566331f
khhwang <khhwang78@gmail.com>
2015-01-30 15:53:16 +0900

    rebuild

--- QuantLib/Debug/Python_test.py
+++ QuantLib/Debug/Python_test.py
@@ -3,17 +3,19 @@ import xml.etree.ElementTree as ET
 
 #Parse
 QL = pm.QL('TEST QL')
-doc = ET.XML(QL.calc())
+#doc = ET.XML(QL.calc())
 
-print "tag=" + doc.tag
-print "text=" + doc.text
+#print "tag=" + doc.tag
+#print "text=" + doc.text
 
 #Create
-root = ET.ElementTree(doc)
-xml_input = ET.tostring(doc, encoding='utf8', method='xml')
 
-QL = pm.QL(xml_input)
+# root = ET.ElementTree(doc)
+# xml_input = ET.tostring(doc, encoding='utf8', method='xml')
+#
+# QL = pm.QL(xml_input)
 print QL.calc()
 
 
 a = input("press Enter")
+
--- QuantLib/PythonModule/pyCall.cpp
+++ QuantLib/PythonModule/pyCall.cpp
@@ -7,26 +7,26 @@ pyCall::pyCall(std::string inputXML) {
 	inputXML_ = inputXML;
 	isError_ = false;
 
-	//Parse XML INPUT
-	XMLDocument doc;
-	doc.Parse(inputXML_.c_str());
-	XMLElement* root = doc.FirstChildElement( "root" );
-	std::string funcName = static_cast<XMLElement*>( root->FirstChildElement("func_root") )->Attribute("name");
+	////Parse XML INPUT
+	//XMLDocument doc;
+	//doc.Parse(inputXML_.c_str());
+	//XMLElement* root = doc.FirstChildElement( "root" );
+	//std::string funcName = static_cast<XMLElement*>( root->FirstChildElement("func_root") )->Attribute("name");
 
-	try
-	{
-		//FunctorFactory::instance().GetFunctor( funcName )->Run( root->FirstChildElement( "param_root" ) );
-	}
-	catch( const std::exception& e )
-	{
-		isError_ = true;
-		retXML_ = makeErrorXML( std::string(e.what()) );
-	}
-	catch( ... )
-	{
-		isError_ = true;
-		retXML_ = makeErrorXML( std::string( "unknown error") );
-	}
+	//try
+	//{
+	//	FunctorFactory::instance().GetFunctor( funcName )->Run( root->FirstChildElement( "param_root" ) );
+	//}
+	//catch( const std::exception& e )
+	//{
+	//	isError_ = true;
+	//	retXML_ = makeErrorXML( std::string(e.what()) );
+	//}
+	//catch( ... )
+	//{
+	//	isError_ = true;
+	//	retXML_ = makeErrorXML( std::string( "unknown error") );
+	//}
 
 }
 
