5a886481e1af5d271285cb66daa97611e0d38fad
khhwang <khhwang78@gmail.com>
2015-02-02 13:54:53 +0900

    minor

--- QuantLib/PythonModule/Main.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <OAIdl.h>
-
-#include "FiccDerivatives_DLL.h"
-
-FICCDERIVATIVES_ENTRY int __stdcall VBA_Call( BSTR data );
-FICCDERIVATIVES_ENTRY int __stdcall VBA_CallByFile( BSTR file_name, bool useProxy, bool calcGreek, bool showDetail );
